7bdc794a2e75eb8736ce1bdea8dbf06f3567f836
BuildTools <unconfigured@null.spigotmc.org>
2017-03-22 15:28:56 -0600

    first commit

---
+++ GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/City.cpp" "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/City.cpp.o"
+  "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/County.cpp" "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/County.cpp.o"
+  "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/CountyUserInterface.cpp" "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/CountyUserInterface.cpp.o"
+  "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/Menu.cpp" "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/Menu.cpp.o"
+  "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/MenuOption.cpp" "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/MenuOption.cpp.o"
+  "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/Nation.cpp" "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/Nation.cpp.o"
+  "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/NationUserInterface.cpp" "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/NationUserInterface.cpp.o"
+  "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/Region.cpp" "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/Region.cpp.o"
+  "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/State.cpp" "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/State.cpp.o"
+  "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/StateUserInterface.cpp" "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/StateUserInterface.cpp.o"
+  "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/UserInterface.cpp" "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/UserInterface.cpp.o"
+  "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/Utils.cpp" "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/Utils.cpp.o"
+  "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/World.cpp" "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/World.cpp.o"
+  "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/WorldUserInterface.cpp" "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/WorldUserInterface.cpp.o"
+  "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/main.cpp" "/cygdrive/c/Users/manab/Desktop/Repositories/1440myRespositories/GeoRegions/cmake-build-debug/CMakeFiles/GeoRegions.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
